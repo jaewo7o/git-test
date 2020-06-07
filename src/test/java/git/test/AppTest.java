8c460643ff61729af77e4b321175dcdf1c6f3d98
JAEWOO <jaewo7o@naver.com>
2020-06-07 22:35:00 +0900

    test 추가

---
+++ src/test/java/git/test/AppTest.java
@@ -0,0 +1,13 @@
+package git.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @org.junit.jupiter.api.Test
+    void getGreeting() {
+        System.out.println("====>test");
+    }
+}
